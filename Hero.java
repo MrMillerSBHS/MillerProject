009240d300e5e144fb29f5cc36bf56f5e9c25ed3
MrMillerSBHS <50634721+MrMillerSBHS@users.noreply.github.com>
2019-05-14 13:38:41 -0400

    Create Hero.java

---
+++ Hero.java
@@ -0,0 +1,24 @@
+public class Hero
+{
+  private int x;
+  private int y;
+  private int health;
+  
+  public Hero()
+  {
+    x = 10;
+    y = 250;
+    health = 100;
+  }
+  
+  public int getX()
+  {
+    return x;
+  }
+  
+  public int getY()
+  {
+    return y;
+  }
+
+}
